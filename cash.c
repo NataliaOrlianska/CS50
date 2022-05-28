19d745a5b3bf18360aea00e6e3dcfb822959a072
NataliaOrlianska <106430668+NataliaOrlianska@users.noreply.github.com>
2022-05-28 21:56:23 +0300

    Create cash.c

---
+++ cash.c
@@ -0,0 +1,39 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void cash(void);
+
+int get_cents (void){
+    int cents;
+    
+    do
+    {
+        cents = get_int("Change owed: ");
+    }
+    while (cents < 1 || cents > 99);
+    
+    return cents;
+}
+
+int calculate_quarters (int cents){
+     return (cents/25);
+}
+
+int calculate_dimes (int cents){
+     return (cents%25/10);
+}
+
+int calculate_nickels (int cents){
+     return (cents%25%10/5);
+}
+
+int calculate_pennies (int cents){
+     return (cents%25%10%5);
+}
+
+int main(void)
+{
+    int cash = get_cents();
+    printf("%d\n",calculate_quarters(cash)+calculate_dimes(cash)+calculate_nickels(cash)+calculate_pennies(cash));
+    
+}
